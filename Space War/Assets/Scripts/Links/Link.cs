9cb1ad1430ee8b25abfb6e2241d792af11959609
EDCG1995 <b00129517@mytudublin.ie>
2020-11-28 17:23:13 +0000

    Final Alpha Update

---
+++ Space War/Assets/Scripts/Links/Link.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+using UnityEngine.UI;
+
+public class Link : MonoBehaviour 
+{
+
+	public void GitHubRepositoryLink()
+	{
+#if !UNITY_EDITOR
+		openWindow("https://github.com/EDCG1995/Space_War");
+#endif
+	}
+
+	[DllImport("__Internal")]
+	private static extern void openWindow(string url);
+
+}
